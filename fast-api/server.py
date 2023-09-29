5724dcd4967ed348592890217a507595cefbe5a1
evgeniy <cononow.jenya2015@gmai.com>
2023-09-29 10:48:45 +0500

    minor changes

--- fast-api/server.py
+++ fast-api/server.py
@@ -1,23 +1,7 @@
-import json
 import redis
-import uuid
-
 import uvicorn
-
-import pandas as pd
-import numpy as np
-from contextlib import asynccontextmanager
-from typing import Union, Annotated, List, Set
-
-import requests
-import torch
-from fastapi import FastAPI, Body, UploadFile, File, HTTPException, Form
-from pydantic import BaseModel, Field
-from util import dot_score
-from resources import *
+from fastapi import FastAPI
 from config import *
-from entourage_search import author_preparation, async_get_data_from_list
-from visualization import graph_visualization, graph_data_preparation, create_edges, create_nodes
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
