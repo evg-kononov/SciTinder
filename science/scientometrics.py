36dacc9422d8e3c1adc52677921d8fc8fc138f02
evg-kononov <cononow.jenya2015@gmail.com>
2023-10-06 16:18:52 +0500

    add scientometrics

--- science/scientometrics.py
+++ science/scientometrics.py
@@ -9,28 +9,69 @@ from tqdm import tqdm
 from itertools import repeat
 from util import *
 from visualization import *
+from multiprocessing import Pool
 
 
-def entourage_coverage(author_df, organization_df, adjacency_list, organization_name=None, k=5):
-    """
+def get_required_idxs(author_df, organization_df, organization_name):
+    """Позволяет получить индексы авторов, которые входят в указанную организацию
 
     :param author_df: сущность "author"
     :param organization_df: сущность "organization"
-    :param adjacency_list: список смежности (словарь, где key - индекс автора, value - индексы соавторов)
     :param organization_name: название организации для которой считается статистика
+    :return: required_idxs: список требуемых индексов
+    """
+
+    required_idx = organization_df[organization_df["name"] == organization_name].index.values[0] + 1
+    author_df = author_df[author_df["organization_id"] == float(required_idx)]
+    required_idxs = author_df.index.values + 1
+    return required_idxs
+
+
+def entourage_coverage(adjacency_list, required_idxs=None, k=5):
+    """
+    :param adjacency_list: список смежности (словарь, где key - индекс автора, value - индексы соавторов)
+    :param required_idxs: список требуемых индексов авторов для подсчета статистики
+    :param k: порядок окружение, вплоть до которого производится расчет статистики
     :return:
     """
 
-    if organization_name:
-        required_idx = organization_df[organization_df["name"] == organization_name].index.values[0] + 1
-        author_df = author_df[author_df["organization_id"] == float(required_idx)]
-        required_idxs = author_df.index.values + 1
+    if required_idxs is None:
+        required_idxs = list(adjacency_list.keys())
 
     source_entourages = []
     for source_id in tqdm(required_idxs):
         tree = find_entourage_for_stats(adjacency_list, source_id, k)
         source_entourages.append(tree)
     return source_entourages
+
+
+def handshake_distribution(adjacency_list, required_idxs=None, k=10):
+    """
+
+    :param adjacency_list: список смежности (словарь, где key - индекс автора, value - индексы соавторов)
+    :param required_idxs: список требуемых индексов авторов для подсчета статистики
+    :param k: порядок окружение, вплоть до которого производится расчет статистики (количество bins в распределении)
+    :return:
+    """
+    if required_idxs is None:
+        required_idxs = list(adjacency_list.keys())
+
+    handshake_number = []
+    for source_id in tqdm(required_idxs):
+        with Pool() as pool:
+            temp = list(pool.starmap(
+                parallel_find_tree_from_list,
+                zip(repeat(adjacency_list), repeat(source_id), list(adjacency_list.keys()), repeat(k // 2))
+            ))
+        handshake_number.append(temp)
+        """
+        for target_id in list(adjacency_list.keys()):
+            source_tree, target_tree, inter = find_tree_from_list(adjacency_list, source_id, target_id, k // 2)
+            if inter is not None:
+                handshake_number.append(len(source_tree.keys()) + len(target_tree.keys()))
+        """
+
+    return handshake_number
 
 
 if __name__ == "__main__":
@@ -48,9 +89,17 @@ if __name__ == "__main__":
     embedding_idxs = torch.load(embedding_idxs_path)
 
     author_df = author_df.iloc[embedding_idxs - 1]
+    organization_name = "PAWLIN Technologies LLC"
 
-    source_entourages = entourage_coverage(author_df, organization_df, adjacency_list, "PAWLIN Technologies LLC", 5)
-    plot_entourage_coverage(source_entourages)
+    required_idxs = get_required_idxs(author_df, organization_df, organization_name)
+
+    #source_entourages = entourage_coverage(adjacency_list, required_idxs, 5)
+    #plot_entourage_coverage(source_entourages)
+
+    handshake_number = handshake_distribution(adjacency_list, required_idxs)
+
+
+
 
 """
 
--- science/visualization.py
+++ science/visualization.py
@@ -1,20 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_entourage_coverage(source_entourages: list):
+def plot_entourage_coverage(source_entourages: list,  plt_style="seaborn-bright", font_size=12, dpi=500):
     """Строит график зависимости порядка окружения от количества находящихся в нем ученых
 
     :param source_entourages: список формы [k, n], где k - индекс окружения, n - индекс ученого для которого посчитана статистика
+    :param plt_style: стиль графика (можно выбрать из matplotlib plot styles)
+    :param font_size: размер шрифта на графике
+    :param dpi: качество изображение в DPI
     :return: график
     """
+    plt.style.use(plt_style)
+    plt.rcParams["font.size"] = f"{font_size}"
+
     source_entourages = np.array(source_entourages)
     mean = np.mean(source_entourages, axis=0)
     std = np.std(source_entourages, axis=0)
     x = list(range(1, len(mean) + 1))
-    plt.plot(x, mean)
-    plt.fill_between(x, mean - std, mean + std, alpha=0.3)
-    plt.xlabel("Окружение")
-    plt.xlabel("Охват")
-    plt.xticks(x)
+
+    fig, ax = plt.subplots(1, 1, figsize=(7, 5), dpi=dpi)
+    ax.plot(x, mean)
+    ax.fill_between(x, mean - std, mean + std, alpha=.1)
+    ax.set_xlabel("Окружение")
+    ax.set_ylabel("Охват")
+    ax.set_xticks(x)
+    ax.grid(linestyle="--")
     plt.show()
 
