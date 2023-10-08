811c6f6f56384287a00d11b3f47777e8d1151a45
Evgeniy <cononow.jenya2015@gmail.com>
2023-10-08 13:55:20 +0500

    add plot of handshake distribution

--- science/visualization.py
+++ science/visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 def plot_entourage_coverage(source_entourages: list,  plt_style="seaborn-bright", font_size=12, dpi=500):
     """Строит график зависимости порядка окружения от количества находящихся в нем ученых
@@ -25,5 +26,26 @@ def plot_entourage_coverage(source_entourages: list,  plt_style="seaborn-bright"
     ax.set_ylabel("Охват")
     ax.set_xticks(x)
     ax.grid(linestyle="--")
+    plt.show()
+
+
+def plot_handshake_distribution(handshake_number: list,  plt_style="seaborn-bright", font_size=12, dpi=500):
+    """Строит график зависимости порядка окружения от количества находящихся в нем ученых
+
+    :param handshake_number: список формы [k], содержащий количество рукопожатий до знакомства
+    :param plt_style: стиль графика (можно выбрать из matplotlib plot styles)
+    :param font_size: размер шрифта на графике
+    :param dpi: качество изображение в DPI
+    :return: график
+    """
+    plt.style.use(plt_style)
+    plt.rcParams["font.size"] = f"{font_size}"
+
+    handshake_number = np.array(handshake_number)
+
+    fig, ax = plt.subplots(1, 1, figsize=(7, 5), dpi=dpi)
+    sns.histplot(data=handshake_number, ax=ax, stat="percent")
+    ax.set_xlabel("Длина пути")
+    ax.set_ylabel("Вероятность, %")
     plt.show()
 
