d35b7aa5b929fdc6dc2e6a9199b80879920e6e76
evg-kononov <cononow.jenya2015@gmail.com>
2023-10-27 09:20:59 +0500

    update redis

--- fast-api/server.py
+++ fast-api/server.py
@@ -1,14 +1,16 @@
 import redis
 import uvicorn
-from fastapi import FastAPI
+from typing import List, Annotated, Union
+from fastapi import FastAPI, Query
 from config import *
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 app = FastAPI()
 
+players = "Players"
 value = [0]
-@app.get("/counter")
+@app.get("/counter/")
 def counter():
     value[0] += 1
     return value[0]
@@ -16,10 +18,32 @@ def counter():
 
 key = "counter"
 r.set(key, "0")
-@app.get("/counter-redis")
+@app.get("/counter-redis/")
 def counter():
     r.incr(key)
     return r.get(key)
+
+
+@app.get("/leaderboard/range/")
+def view(
+        start: Annotated[int, Query()] = 0,
+        end: Annotated[int, Query()] = -1
+):
+    return r.zrange(players, start, end, desc=True, withscores=True)
+
+
+@app.post("/leaderboard/add/")
+def view(
+        player: Annotated[str, Query()],
+        score: Annotated[int, Query()]
+):
+    return r.zadd(players, {player: score})
+
+
+@app.delete("/leaderboard/remove/")
+def remove(player: Annotated[Union[List[str], None], Query()] = None):
+    return r.zrem(players, *player)
+
 
 
 if __name__ == "__main__":
